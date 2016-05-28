b0d793f19296bc44975c7424c01f668ac32f292b
Garry Ing <garry.ing@gmail.com>
2016-05-27 22:34:01 -0400

    webpack install

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const webpack = require('webpack')
+const path = require('path')
+
+module.exports = {
+  context: path.join(__dirname, './_js'),
+  entry: './main.js',
+  output: {
+    path: path.join(__dirname, './js'),
+    filename: 'main.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loaders: [
+          'babel-loader'
+        ]
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': { NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development') }
+    })
+  ],
+  devServer: {
+    contentBase: './_js',
+    hot: true
+  }
+}
---
+++ webpack.config.production.js
@@ -0,0 +1,14 @@
+const assign = require('lodash/assign')
+const webpack = require('webpack')
+const config = require('./webpack.config')
+
+module.exports = assign({}, config, {
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.DefinePlugin({
+      'process.env': { NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'production') }
+    }),
+    new webpack.optimize.UglifyJsPlugin()
+  ]
+})
