f6f4f2de0e9f04bde1fbffc72db922811552cf92
Garry Ing <garry.ing@gmail.com>
2016-06-19 17:41:35 -0400

    hover stuff

--- _js/main.js
+++ _js/main.js
@@ -1,12 +1,59 @@
 const $ = require('jquery');
+window.jQuery = window.$ = $;
+require('./intent');
 
 (() => {
   const app = {
     init: () => {
       app.scrollHelper();
+      app.imgHelper();
     },
     ele: {
       navigation: $('.navigation'),
+      imageGrid: $('#image-grid'),
+    },
+    imgHelper: () => {
+      const searchID = '017668023985580936890:l2eosepcyty';
+      const searchAPIkey = 'AIzaSyDKm3GPSVDzltXsAcZTB8VeYdrL0N2ZZhc';
+
+      function makeNewPosition() {
+        // Get viewport dimensions (remove the dimension of the div)
+        const h = $(window).height();
+        const w = $(window).width();
+        const nh = Math.floor(Math.random() * h) / h * 100;
+        const nw = Math.floor(Math.random() * w) / w * 100;
+        return [nh, nw];
+      }
+
+      function imageEle(obj) {
+        const pos = makeNewPosition();
+        return `
+          <div class="image-unit" style="top:${pos[0]}%; left:${pos[1]}%">
+            <img src=${obj.link} class="image-item" />
+            <div class="image-item-caption">${obj.link}</div>
+          </div>
+        `;
+      }
+
+      function googleSearch(term) {
+        const googleAPIurl = `https://www.googleapis.com/customsearch/v1?key=${searchAPIkey}&cx=${searchID}&q=${term}&alt=json&searchType=image`;
+        // const googleAPIurl = './js/data.json';
+
+        $.get(googleAPIurl)
+        .done((results) => {
+          app.ele.imageGrid.empty().append(results.items.map(imageEle)).addClass('active');
+        });
+      }
+
+      $('[data-metaimage]').hoverIntent(
+        (ele) => {
+          const term = $(ele.currentTarget).data('metaimage');
+          googleSearch(term);
+        },
+        () => {
+          app.ele.imageGrid.removeClass('active');
+        }
+      );
     },
     scrollHelper: () => {
       let lastScrollY = 0;
