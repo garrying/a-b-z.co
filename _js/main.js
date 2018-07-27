a59129f2dd1a8bace9cbadcb9fec90d0595a5ffb
Garry Ing <garry.ing@gmail.com>
2018-07-27 09:37:51 -0400

    new key

--- _js/main.js
+++ _js/main.js
@@ -3,7 +3,7 @@ const vhCheck = require('vh-check');
 const IdleJs = require('idle-js');
 
 const searchID = '017668023985580936890:l2eosepcyty';
-const searchAPIkey = 'AIzaSyDKm3GPSVDzltXsAcZTB8VeYdrL0N2ZZhc';
+const searchAPIkey = 'AIzaSyDJuy-kp0l0J5eyExT3rdr68dpx11pJlz8';
 let idleCounter = 5;
 let intervalID;
 
